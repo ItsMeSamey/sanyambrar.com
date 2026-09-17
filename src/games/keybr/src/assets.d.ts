dc27736bbee64d7d1264dc4be985dcc4cae947d8
ItsMeSamey <sameychain5041@gmail.com>
2026-09-18 00:47:04 +1000

    refactor: consolidate ambient declarations

--- src/games/keybr/src/assets.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.data" {
-  const url: string;
-  export default url;
-}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+declare module "*.data" {
+  const url: string;
+  export default url;
+}
+
+declare module 'monaco-editor/language/json/monaco.contribution';
