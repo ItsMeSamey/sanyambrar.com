d93542e33b9fed5adee85a660f49f0e0d85dd88b
ItsMeSamey <sameychain5041@gmail.com>
2026-09-19 12:05:32 +1000

    build(site): regenerate static assets

---
+++ docs/site-chunks/register-DY21h9yp.js
@@ -0,0 +1,1 @@
+import{C as e}from"./site-app-BWGACyWg.js";import{t}from"./_.contribution-BCkR6AMw.js";t({id:`css`,extensions:[`.css`],aliases:[`CSS`,`css`],mimetypes:[`text/css`],loader:()=>e(()=>import(`./css-DIMkf-bt.js`),[])});
---
+++ docs/site-chunks/register-rEeBNJuG.js
@@ -0,0 +1,1 @@
+import{C as e}from"./site-app-BWGACyWg.js";import{t}from"./_.contribution-BCkR6AMw.js";t({id:`rust`,extensions:[`.rs`,`.rlib`],aliases:[`Rust`,`rust`],loader:()=>e(()=>import(`./rust-DdL9SqIa.js`),[])});
