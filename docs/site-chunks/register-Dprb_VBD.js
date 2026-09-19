9798ff2821fac36945a722c12045dc27cdb4048d
ItsMeSamey <sameychain5041@gmail.com>
2026-09-20 05:26:46 +1000

    build(site): regenerate static assets

---
+++ docs/site-chunks/register-Dprb_VBD.js
@@ -0,0 +1,1 @@
+import{C as e}from"./site-app-BFjVMiRV.js";import{t}from"./_.contribution-CyQXSzl5.js";t({id:`rust`,extensions:[`.rs`,`.rlib`],aliases:[`Rust`,`rust`],loader:()=>e(()=>import(`./rust-DdL9SqIa.js`),[])});
