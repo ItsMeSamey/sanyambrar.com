592b1bb77cf3cfb9bf448e600025521b050446d3
ItsMeSamey <sameychain5041@gmail.com>
2026-09-20 06:22:09 +1000

    build(site): regenerate static assets

--- docs/site-chunks/register-BKu2Z8ub.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./site-app-BFjVMiRV.js";import{t}from"./_.contribution-CyQXSzl5.js";t({id:`shell`,extensions:[`.sh`,`.bash`],aliases:[`Shell`,`sh`],loader:()=>e(()=>import(`./shell-CC2rA5mh.js`),[])});
